cd4b893a019af0fc6d071e2e08f44da69bbc2eff
HamimBhai742 <mdhamim5088@gmail.com>
2026-03-02 10:03:15 +0600

    update

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,8 @@ const nextConfig: NextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*', // any frontend call to /api/*
-        destination: 'http://16.170.226.171:5001/api/v1/:path*', // your backend
+        source: '/api/v1/:path*',
+        destination: 'http://16.170.226.171:5001/api/v1/:path*',
       },
     ]
   },
